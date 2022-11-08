c07b422361120bf564d6e5d342aa6c593f8598fe
David Skripnikov <davidskrip1020@gmail.com>
2022-11-08 15:46:59 -0500

    Modified Global Control
    Changed parameters to private
    Added mini game scores to non-destruct game object
    Added methods to modify and access parameters and scores

--- Assets/Scripts/Global_Control/GlobalControl.cs
+++ Assets/Scripts/Global_Control/GlobalControl.cs
@@ -6,9 +6,15 @@ public class GlobalControl : MonoBehaviour
 {
     public static GlobalControl Instance;
 
-    public float water;
-    public float air;
-    public float forrest;
+    // GLOBAL PARAMETERS
+    private float water = 100f;
+    private float air = 100f;
+    private float forrest = 100f;
+
+    // MINI-GAME SCORES
+    private int deforestation = 0;
+    private int waterConservation = 0;
+    private int energyConservation = 0;
 
     void Awake()
     {
@@ -21,5 +27,52 @@ public class GlobalControl : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void saveParamaters(float w, float a, float f)
+    {
+        Instance.water = w;
+        Instance.air = a;
+        Instance.forrest = f;
+    }
+
+    public float getWater()
+    {
+        return Instance.water;
+    }
+    public float getAir()
+    {
+        return Instance.air;
+    }
+    public float getForrest()
+    {
+        return Instance.forrest;
+    }
+
+    public void setDeforestationScore(int score)
+    {
+        Instance.deforestation = score;
+    }
+    public int getDeforestaionScore()
+    {
+        return Instance.deforestation;
+    }
+
+    public void setWaterConservationScore(int score)
+    {
+        Instance.waterConservation = score;
+    }
+    public int getWaterConservationScore()
+    {
+        return Instance.waterConservation;
+    }
+
+    public void setEnergyConservationScore(int score)
+    {
+        Instance.energyConservation = score;
+    }
+    public int getEnergyConservation()
+    {
+        return Instance.energyConservation;
     }
 }
