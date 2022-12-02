b27e07a9437ef10f29346555a7d9a96d93c550b7
David Skripnikov <davidskrip1020@gmail.com>
2022-12-01 23:57:27 -0500

    Set up scenes in order
    Set up scenes in order for gameplay
    Added buttons to access mini games from home base
    Set up player account to write to the database when score for minigames updated

--- Assets/Scripts/Login/Player_Account.cs
+++ Assets/Scripts/Login/Player_Account.cs
@@ -8,6 +8,7 @@ public class Player_Account : MonoBehaviour
     public static Player_Account Instance;
 
     public string username;
+    public string password;
 
     public string[] databaseAccount;
 
@@ -51,18 +52,10 @@ public class Player_Account : MonoBehaviour
         return Instance.gameScore;
     }
 
-    public void setDeforestationScore(int score)
-    {
-        Instance.deforestation = score;
-    }
-    public int getDeforestaionScore()
-    {
-        return Instance.deforestation;
-    }
-
     public void setWaterConservationScore(int score)
     {
         Instance.waterConservation = score;
+        writeDatabase();
     }
     public int getWaterConservationScore()
     {
@@ -72,10 +65,21 @@ public class Player_Account : MonoBehaviour
     public void setEnergyConservationScore(int score)
     {
         Instance.energyConservation = score;
+        writeDatabase();
     }
     public int getEnergyConservation()
     {
         return Instance.energyConservation;
+    }
+
+    public void setDeforestationScore(int score)
+    {
+        Instance.deforestation = score;
+        writeDatabase();
+    }
+    public int getDeforestaionScore()
+    {
+        return Instance.deforestation;
     }
 
     public bool verifyAccount(string uName)
@@ -103,8 +107,17 @@ public class Player_Account : MonoBehaviour
     public void readData()
     {
         Instance.username = databaseAccount[0];
+        Instance.password = databaseAccount[1];
         Instance.waterConservation = Int32.Parse(databaseAccount[2]);
         Instance.energyConservation = Int32.Parse(databaseAccount[3]);
         Instance.deforestation = Int32.Parse(databaseAccount[4]);
+    }
+
+    public void writeDatabase()
+    {
+        string wGame = Instance.waterConservation.ToString();
+        string eGame = Instance.energyConservation.ToString();
+        string dGame = Instance.deforestation.ToString();
+        Instance.GetComponent<WriteData>().WriteMyData(username, password, wGame, eGame, dGame);
     }
 }
