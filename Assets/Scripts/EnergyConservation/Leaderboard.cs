a1e1bfc2b1034b42ce08860085d91ae693a0defa
Axel Aquino <aaquinojimenez@ufl.edu>
2022-12-06 16:30:32 -0500

    adusted home button functionality and added exit button to home base

--- Assets/Scripts/EnergyConservation/Leaderboard.cs
+++ Assets/Scripts/EnergyConservation/Leaderboard.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
-    [SerializeField] GameObject leaderboard;
+    [SerializeField] GameObject display;
+    public TMP_Text userScores;
+    private WriteData data;
+    public static bool hasClicked = false;
 
-    private void Start()
+
+    public void Start()
     {
-        leaderboard.SetActive(false);
+        display.SetActive(false);
+        data = GameObject.Find("PLAYER_ACCOUNT").GetComponent<WriteData>();
     }
     public void Display()
     {
-        leaderboard.SetActive(true);
+        //todo dont allow player to access leaderboard again when already clicked
+        if (hasClicked)
+        {
+            display.SetActive(false);
+            Time.timeScale = 1;
+            userScores.text = "";
+            hasClicked = false;
+            return;
+        }
+        hasClicked = true;
+        Time.timeScale = 0;
+        display.SetActive(true);
+
+        List<string[]> alldata = data.GetAllData();
+        //int score;
+
+        // Debug.Log(alldata.Count);
+        var itemMoved = false;
+        do
+        {
+            itemMoved = false;
+            for (int i = 1; i < alldata.Count - 1; i++)
+            {
+                if (int.Parse(alldata[i][2]) < int.Parse(alldata[i + 1][2]))
+                {
+                    string[] higherArray = alldata[i + 1];
+                    alldata[i + 1] = alldata[i];
+                    alldata[i] = higherArray;
+                    itemMoved = true;
+                }
+            }
+
+        } while (itemMoved);
+        int count = 0;
+        for (int i = 1; i < alldata.Count; i++)
+        {
+            ++count;
+            userScores.text += i + ". " + alldata[i][0] + ": " + alldata[i][2] + "\n";
+            if (count == 10)
+            {
+                break;
+            }
+        }
     }
 }
